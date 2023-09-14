ae4f54f125e345758daf31c736ac501aa7ca8f4f
jonathanvra <jonathan.vra@gmail.com>
2023-09-13 19:46:22 -0500

    feat: :lipstick: I changed the button icons

--- src/components/QrComponent/QrComponent.tsx
+++ src/components/QrComponent/QrComponent.tsx
@@ -117,7 +117,11 @@ function QrComponent() {
         </button>
         <div className={`alert__container ${showAlert ? 'show' : ''}`}>
           <div className={`alert ${showAlert ? 'show' : ''}`}>
-          {showAlert ? (text.trim() === '' ? 'Ingresa una URL' : 'Escribe una URL válida para generar el código QR') : ''}
+            {showAlert
+              ? text.trim() === ''
+                ? 'Ingresa una URL'
+                : 'Escribe una URL válida para generar el código QR'
+              : ''}
           </div>
         </div>
         {qrCodeText && (
@@ -132,12 +136,34 @@ function QrComponent() {
               <button
                 className='qrComponent__buttons-button'
                 onClick={handleDownloadQR}>
-                Descargar
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  height='24'
+                  viewBox='0 -960 960 960'
+                  width='24'>
+                  <path
+                    d='M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200Z'
+                    fill='currentColor'
+                  />
+                  <path
+                    d='M240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z'
+                    fill='currentColor'
+                  />
+                </svg>
               </button>
               <button
                 className='qrComponent__buttons-button'
                 onClick={resetApp}>
-                Reiniciar
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  height='24'
+                  viewBox='0 -960 960 960'
+                  width='24'>
+                  <path
+                    d='M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z'
+                    fill='currentColor'
+                  />
+                </svg>
               </button>
             </div>
           </div>
